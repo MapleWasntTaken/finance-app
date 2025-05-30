4a1f84165f6f07733ebe301f9dd4b16d48f33d20
Josh Moreira <joshuamoreirapersonal@gmail.com>
2025-05-30 01:34:16 -0400

    garbage push

--- src/main/java/com/financialapp/financialapp/FinancialappApplication.java
+++ src/main/java/com/financialapp/financialapp/FinancialappApplication.java
@@ -2,8 +2,9 @@ package com.financialapp.financialapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class FinancialappApplication {
 
 	public static void main(String[] args) {
